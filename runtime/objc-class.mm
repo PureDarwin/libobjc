e1f1fb8ee0d2e6bf6247b8843131ae51d9a6409f
William Kent <wjk@sunsol.me>
2018-09-04 19:21:38 -0400

    Fix warnings

--- runtime/objc-class.mm
+++ runtime/objc-class.mm
@@ -564,10 +564,10 @@ void fixupCopiedIvars(id newObject, id oldObject)
                 while ((byte = *weakLayout++)) {
                     unsigned skips = (byte >> 4);
                     unsigned weaks = (byte & 0x0F);
-                    newPtr += skips, oldPtr += skips;
+                    newPtr += skips; oldPtr += skips;
                     while (weaks--) {
                         objc_copyWeak(newPtr, oldPtr);
-                        ++newPtr, ++oldPtr;
+                        ++newPtr; ++oldPtr;
                     }
                 }
             }
